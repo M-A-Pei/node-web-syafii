b78199f266b944081cc05163e345c86b6f0121a5
Muhammad Abdullah Syafii <syafii2006@gmail.com>
2024-03-29 09:21:12 +0700

    added bootstrap icons/form/title/data folder/contactjs

--- app.js
+++ app.js
@@ -7,23 +7,16 @@ const app = express()
 const port = 3000
 const os = require('os');
 const path = require('path')
+const contact = require('./utils/contacts');
 
 app.set("view engine", "ejs")
 app.use(expressEjsLayouts)
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap-icons/font')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
-const orang = [
-    {
-        nama: "syafii",
-        umur: 17
-    },
-    {
-        nama: "hamzah",
-        umur: 16
-    }
-]
+const orang = contact.tampilList();
 
 app.get('/', function (req, res) {
     res.render("index", {nama: os.userInfo().username, layout:"layouts/main"})
@@ -33,6 +26,10 @@ app.get('/about', function (req, res) {
 })
 app.get('/contact', function (req, res) {
     res.render("contact", {orang, layout:"layouts/main"})
+})
+app.get('/contact/:nama', (req, res) => {
+    const nama = req.params.nama;
+    res.render("detail", {layout: "layouts/main", nama})
 })
 
 app.listen(port, ()=>{
---
+++ utils/contacts.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const validator = require('validator');
+
+if(!fs.existsSync("./data")){
+    fs.mkdirSync("./data");
+}
+
+if(!fs.existsSync("./data/contacts.json")){
+    fs.writeFileSync("./data/contacts.json", "[]");
+}
+
+const simpanContact = (data) => {
+    let file = JSON.parse(fs.readFileSync("./data/contacts.json", "utf-8"));
+    let duplikat = file.find((contacts)=>{return contacts.email === data.email});
+
+    if(!validator.isEmail(data.email)){console.log(error(`ini bukan email`)); rl.close(); return false}
+
+    if(duplikat) {
+        console.log(error(`email sudah ada`));
+        return false;
+    }
+
+    file.push(data);
+    fs.writeFileSync("./data/contacts.json", JSON.stringify(file));
+    console.log(success(`terimakasih untuk informasi mu \n`));
+}
+
+const tampilList = () =>{
+    let file = JSON.parse(fs.readFileSync("./data/contacts.json", "utf-8"));
+    return file;
+}
+
+const deleteList = (nama) =>{
+    let file = JSON.parse(fs.readFileSync("./contacts.json", "utf-8"));
+    file.forEach((element, i) => {
+        if(element.nama === nama){
+            file.splice(i, 1);
+            fs.writeFileSync("./contacts.json", JSON.stringify(file));
+            console.log(chalk `{bgRed.black ${element.nama} telah di hapus dari daftar contact}`);
+        }
+    });
+}
+
+
+module.exports = {simpanContact, tampilList, deleteList};
